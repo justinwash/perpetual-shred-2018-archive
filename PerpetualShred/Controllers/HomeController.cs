5c3e4380cbc345d8f71d1bd6213d876cec7739fa
Trifectuh <trifectuh@icloud.com>
2017-10-09 20:20:16 -0500

    Initial commit

---
+++ PerpetualShred/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PerpetualShred.Models;
+
+namespace PerpetualShred.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "A place to watch and share videos that get you pumped to be stoked.";
+
+            return View();
+        }
+
+        public IActionResult Submit()
+        {
+            ViewData["Message"] = "The crawler can only do so much...";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
