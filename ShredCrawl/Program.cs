487744718f554b6eba174b24bf20e0fbed638810
Trifectuh <trifectuh@icloud.com>
2017-10-12 00:24:05 -0500

    on our way to a shred crawler

---
+++ ShredCrawl/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace ShredCrawl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex rx = new Regex(@"youtube.com/embed/.*$");
+            
+            var html = "https://www.pinkbike.com/news/movies-for-your-monday-october9-2017.html";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//iframe");
+
+            /*
+            foreach ( HtmlNode node in nodes)
+             {
+                MatchCollection matches = rx.Matches(node.OuterHtml);
+                Console.WriteLine("{0} matches found in:\n   {1}",
+                         matches.Count,
+                         text);
+            }
+            */
+
+            Console.WriteLine(nodes[1].OuterHtml);
+            Match matches = rx.Match(nodes[1].OuterHtml);
+
+            if (matches.Success)
+            {
+                string input = matches.Value;
+                int index = input.IndexOf("\"");
+                if (index > 0)
+                    input = input.Substring(0, index);
+                Console.WriteLine(input);
+            }
+
+          
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
