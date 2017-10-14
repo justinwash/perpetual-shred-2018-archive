29e67dff5c2291a6e19a64db1406e0a973710d03
Trifectuh <trifectuh@icloud.com>
2017-10-14 18:58:15 -0500

    youtube and vimeo work now
    with all variants of their urls.
    
    pinkbike still a work in progress

--- ShredCrawl/Program.cs
+++ ShredCrawl/Program.cs
@@ -10,26 +10,48 @@ namespace ShredCrawl
         static void Main(string[] args)
         {
 
-            Regex rx = new Regex(@"embed/([^']*)");
-            
+            Regex youtubeMatch = new Regex("youtu(?:.be|be.com)/(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]).+?(?=\")");
+            Regex vimeoMatch = new Regex("(player.)?(vimeo.com)/(video/)?([0-9]+)");
+            Regex pinkMatch = new Regex("(player.)?(vimeo.com)/(video/)?([0-9]+)");
+
             var html = "https://www.pinkbike.com/news/movies-for-your-monday-october9-2017.html";
 
             HtmlWeb web = new HtmlWeb();
 
             var htmlDoc = web.Load(html);
 
-            var nodes = htmlDoc.DocumentNode.SelectNodes("//iframe");
+            var ytNodes = htmlDoc.DocumentNode.SelectNodes("//iframe");
+            var pbNodes = htmlDoc.DocumentNode.SelectNodes("//video");
 
-            foreach (HtmlNode node in nodes) {
-                Match matches = rx.Match(node.OuterHtml);
-                if (matches.Success)
+            foreach (HtmlNode node in ytNodes)
+            {
+                Match youtubeMatches = youtubeMatch.Match(node.OuterHtml);
+                Match vimeoMatches = vimeoMatch.Match(node.OuterHtml);
+
+                if (youtubeMatches.Success)
                 {
-                    string input = matches.Value;
-                    int index = input.IndexOf("\"");
-                    if (index > 0)
-                        input = input.Substring(6, 11);
-                    Console.WriteLine(input);
+                    string input = youtubeMatches.Value;
+                    Console.WriteLine("Youtube match: " + input);
                     
+                }
+
+                if (vimeoMatches.Success)
+                {
+                    string input = vimeoMatches.Value;
+                    Console.WriteLine("Vimeo match: " + input);
+
+                }
+            }
+
+            foreach (HtmlNode node in ytNodes)
+            {
+                Match pinkMatches = pinkMatch.Match(node.OuterHtml);
+
+                if (pinkMatches.Success)
+                {
+                    string input = pinkMatches.Value;
+                    Console.WriteLine("PinkBike match: " + input);
+
                 }
             }
 
