a9625ad580b8e112994a3219ac1fcd4e6a132546
Trifectuh <trifectuh@icloud.com>
2017-11-26 21:48:50 -0600

    getting all the pinkbike info
    but cant make the database work on my laptop :(

---
+++ PerpetualShred/Migrations/20171127033903_Laptop.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PerpetualShred.Migrations
+{
+    public partial class Laptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginTitle",
+                table: "WebVid",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VideoService",
+                table: "WebVid",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginTitle",
+                table: "WebVid");
+
+            migrationBuilder.DropColumn(
+                name: "VideoService",
+                table: "WebVid");
+        }
+    }
+}
