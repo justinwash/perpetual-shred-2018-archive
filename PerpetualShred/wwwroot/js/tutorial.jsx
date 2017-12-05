46a07c39237b942ff3ccc8af750c1aca71b575d9
Trifectuh <trifectuh@icloud.com>
2017-12-05 17:48:27 -0600

    more tutorialling
    at the "Updating State" portion on the React.NET tutorial

--- PerpetualShred/wwwroot/js/tutorial.jsx
+++ PerpetualShred/wwwroot/js/tutorial.jsx
@@ -1,13 +1,83 @@
-﻿var CommentBox = React.createClass({
+﻿var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+
         return (
-            <div className="commentBox">
-                Testing React.js integration!
+            <div className="commentList">
+                {commentNodes}
       </div>
         );
     }
 });
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+      </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillMount: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm />
+      </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
+    render: function () {
+        var md = new Remarkable();
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+var data = [
+    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
+    { id: 2, author: "Pete Hunt", text: "This is one comment" },
+    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
+];
+
+
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox url="/comments" />,
     document.getElementById('content')
 );
