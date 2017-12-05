931d2e9c5ad87930491dd7e26fc58f1222fcd4b9
Trifectuh <trifectuh@icloud.com>
2017-12-05 14:36:38 -0600

    Index is now a testbed for React stuff
    might as well

--- PerpetualShred/Startup.cs
+++ PerpetualShred/Startup.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using PerpetualShred.Models;
+
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
+using System;
 
 namespace PerpetualShred
 {
@@ -22,14 +22,16 @@ namespace PerpetualShred
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
             services.AddMvc();
 
             services.AddDbContext<PerpetualShredContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("PerpetualShredContext")));
 
-            
+            return services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +46,27 @@ namespace PerpetualShred
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+                config.AddScript("~/js/tutorial.jsx");
+            });
 
             app.UseStaticFiles();
 
---
+++ PerpetualShred/wwwroot/js/tutorial.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                Testing React.js integration!
+      </div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
