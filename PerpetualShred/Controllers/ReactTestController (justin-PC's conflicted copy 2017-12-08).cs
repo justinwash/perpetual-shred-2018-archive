835791b638a2678d92248a6380e80aa86eb3e81b
Trifectuh <trifectuh@icloud.com>
2017-12-11 18:13:35 -0600

    who knows

---
+++ PerpetualShred/Controllers/ReactTestController (justin-PC's conflicted copy 2017-12-08).cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PerpetualShred.Models;
+
+namespace PerpetualShred.Controllers
+{
+    public class ReactTestController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static ReactTestController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        public IActionResult ReactTest()
+        {
+            return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+    }
+}
