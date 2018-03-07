c13ee04ad93351df7376cf3308418120a158759c
trifectuh <trifectuh@icloud.com>
2018-03-07 00:43:34 -0600

    attempt to fix webpack issues. failed.

--- PerpetualShred/webpack.config.js
+++ PerpetualShred/webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader",
             query: {
-                presets: ["es2015", "stage-1", "react"]
+                presets: ["es2015", "stage-1", "react"],
+                plugins: ["add-module-exports"]
             }
         }]
     }
