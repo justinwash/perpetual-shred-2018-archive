46023a49ac447719394fcf6227159fd41610287c
Trifectuh <justin.wash@pm.me>
2018-05-01 21:03:10 -0500

    Fav removal button (and undo) working.

--- PerpetualShred/Scripts/jsx/AccountComponents/FavRemover.jsx
+++ PerpetualShred/Scripts/jsx/AccountComponents/FavRemover.jsx
@@ -7,16 +7,29 @@ class FavRemover extends Component {
         super(props);
         this.state = {
             favRemoved: false,
+            error: false
         };
         this.removeFav = this.removeFav.bind(this);
+        this.undoRemove = this.undoRemove.bind(this);
     }
 
     removeFav() {
         var vidUrl = this.props.shredVid.playerUrl;
-        axios.get("/Account/RemoveFav?vidUrl=" + vidurl)
+        axios.get("/Account/RemoveFav?vidUrl=" + vidUrl)
             .then(res => {
                 const removalStatus = res.data;
                 this.setState({favRemoved: removalStatus});
+            })
+    }
+    
+    undoRemove(){
+        axios.get("/Account/AddFav?vidUrl=" + this.props.shredVid.playerUrl)
+            .then(res => {
+                const addResult = res.data;
+                this.setState({ favRemoved: !addResult });
+                if (!addResult){
+                    this.setState( { error: true });
+                }
             })
     }
 
@@ -24,14 +37,14 @@ class FavRemover extends Component {
         if (this.state.favRemoved) {
             return (
                 <div id="remove-fav">
-                    REMOVED FROM FAVORITES
+                    <button onClick={this.undoRemove}>REMOVED. UNDO?</button>
                 </div>
             )
         }
         else {
             return (
                 <div id="remove-fav">
-                    <a onClick={this.removeFav}>REMOVE</a>
+                    <button onClick={this.removeFav}>REMOVE</button>
                 </div>
             )
         }
