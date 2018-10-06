77fca5b1962deb4c143f062a8d3feb44f1e222e6
Justin Wash <trifectuh@icloud.com>
2018-10-05 23:36:28 -0700

    update gitignore ,remove unnecessary var

--- PerpetualShred/Randomizer.cs
+++ PerpetualShred/Randomizer.cs
@@ -7,26 +7,25 @@ using PerpetualShred.Models;
 
 namespace PerpetualShred
 {
-    public class Randomizer : Controller
-    {
-        private static string _previousVid;
+	public class Randomizer : Controller
+	{
+		private static string _previousVid;
 
-        public Randomizer()
-        {
-            
-        }
+		public Randomizer()
+		{
 
-        public int RandomVidPicker(List<WebVid> vidList)
-        {
-            if (vidList.Count <= 0) return -1;
-            int id;
-            WebVid vidToPlay;
-            List<string> unwatchedIds = null;
-            vidToPlay = vidList[(new Random().Next(0, vidList.Count))];
-            id = vidToPlay.Id;
-            _previousVid = JsonConvert.SerializeObject(vidToPlay);
+		}
 
-            return id;
-        }
-    }
+		public int RandomVidPicker(List<WebVid> vidList)
+		{
+			if (vidList.Count <= 0) return -1;
+			int id;
+			WebVid vidToPlay;
+			vidToPlay = vidList[(new Random().Next(0, vidList.Count))];
+			id = vidToPlay.Id;
+			_previousVid = JsonConvert.SerializeObject(vidToPlay);
+
+			return id;
+		}
+	}
 }
